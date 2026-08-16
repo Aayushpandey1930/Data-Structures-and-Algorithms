f0fe70ed8edb191b278a611a8077b39552b4d695
Aayush pandey <aayupanvar20@gmail.com>
2026-08-16 22:52:15 +0530

    Time: 129 ms (5.39%) | Memory: 147.4 MB (5.20%) - LeetSync

--- 2721-sum-of-distances/sum-of-distances.cpp
+++ 2721-sum-of-distances/sum-of-distances.cpp
@@ -18,35 +18,69 @@ public:
         // }
         // return ans;
 
-        
+        // int n = nums.size();
+        // vector<long long> ans(n);
+
+        // unordered_map<int, long long> count;
+        // unordered_map<int, long long> sum;
+
+        // // Left side contribution
+        // for (int i = 0; i < n; i++) {
+        //     int x = nums[i];
+
+        //     ans[i] += (long long)i * count[x] - sum[x];
+
+        //     count[x]++;
+        //     sum[x] += i;
+        // }
+
+        // // Reset for right side
+        // count.clear();
+        // sum.clear();
+
+        // // Right side contribution
+        // for (int i = n - 1; i >= 0; i--) {
+        //     int x = nums[i];
+
+        //     ans[i] += sum[x] - (long long)i * count[x];
+
+        //     count[x]++;
+        //     sum[x] += i;
+        // }
+
+        // return ans;
+
         int n = nums.size();
-        vector<long long> ans(n);
 
-        unordered_map<int, long long> count;
-        unordered_map<int, long long> sum;
+        map<int, vector<int>> mp;
 
-        // Left side contribution
         for (int i = 0; i < n; i++) {
-            int x = nums[i];
-
-            ans[i] += (long long)i * count[x] - sum[x];
-
-            count[x]++;
-            sum[x] += i;
+            mp[nums[i]].push_back(i);
         }
 
-        // Reset for right side
-        count.clear();
-        sum.clear();
+        vector<long long> ans(n, 0);
 
-        // Right side contribution
-        for (int i = n - 1; i >= 0; i--) {
-            int x = nums[i];
+        for (auto a : mp) {
+            vector<int> temp = a.second;
+            int m = temp.size();
 
-            ans[i] += sum[x] - (long long)i * count[x];
+            // Prefix sum of positions
+            vector<long long> prefix(m + 1, 0);
 
-            count[x]++;
-            sum[x] += i;
+            for (int i = 0; i < m; i++) {
+                prefix[i + 1] = prefix[i] + temp[i];
+            }
+
+            for (int i = 0; i < m; i++) {
+                // Distance from temp[i] to all positions on the left
+                long long left = 1LL * temp[i] * i - prefix[i];
+
+                // Distance from temp[i] to all positions on the right
+                long long right =
+                    (prefix[m] - prefix[i + 1]) - 1LL * temp[i] * (m - i - 1);
+
+                ans[temp[i]] = left + right;
+            }
         }
 
         return ans;
