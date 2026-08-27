7eab215380c34d1fd211e2e22616689465a629b9
Aayush pandey <aayupanvar20@gmail.com>
2026-08-28 00:33:27 +0530

    Time: 0 ms (100.00%) | Memory: 24.3 MB (69.24%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -1,11 +1,30 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        set<int> st(nums.begin(), nums.end());
-        int ans = k;
-        while(st.count(ans)){
-            ans += k;
+        // method1...
+
+        // set<int> st(nums.begin(), nums.end());
+        // int ans = k;
+        // while(st.count(ans)){
+        //     ans += k;
+        // }
+        // return ans;
+
+        // method2...
+        unordered_map<int, int> mpp;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] % k == 0)
+                mpp[nums[i]]++;
         }
-        return ans;
+
+        int count = 1;
+        int product = 1;
+        while (true) {
+            product = k * count;
+            if (mpp.find(product) == mpp.end())
+                break;
+            else count++;
+        }
+        return product;
     }
 };
