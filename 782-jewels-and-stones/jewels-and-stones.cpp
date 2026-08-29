1379529913a2fd936ec7964c1703e2915f2f9bed
Aayush pandey <aayupanvar20@gmail.com>
2026-08-30 03:00:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (91.66%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        unordered_set<char> st;
-        for(auto x: jewels){
-            st.insert(x);
-        }
+        // unordered_set<char> st;
+        // for(auto x: jewels){
+        //     st.insert(x);
+        // }
+        // int count = 0;
+        // for(auto x: stones){
+        //     if(st.count(x)){
+        //         count++;
+        //     }
+        // }
+        // return count;
         int count = 0;
-        for(auto x: stones){
-            if(st.count(x)){
-                count++;
+        for(int i = 0; i <jewels.size(); i++){
+            for(int j = 0; j <stones.size(); j++){
+                if(jewels[i] == stones[j]){
+                    count++;
+                }
             }
         }
         return count;
