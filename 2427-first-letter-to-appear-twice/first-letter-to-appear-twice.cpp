fb3b927b725fb60aa6dedfce1473f998abffb9e2
Aayush pandey <aayupanvar20@gmail.com>
2026-08-30 02:25:47 +0530

    Time: 2 ms (5.05%) | Memory: 8.4 MB (42.35%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        unordered_set<char> st;
+        for(auto x: s){
+            if(st.count(x)){
+                return x;
+            }
+            else {
+                st.insert(x);
+            }
+        }
+        return '\0';
+    }
+};
